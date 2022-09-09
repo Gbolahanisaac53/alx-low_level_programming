8643846ce7798acd99f8e86deff8ea4ecae40e2c
Gbolahanisaac53 <isaacgbolahan53@gmail.com>
2022-09-09 00:06:33 -0700

    put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
 /**
- *  * main - entry point
- *   * Description: Print a string using followed by a new line
- *    * Return: 0
- *    */
+ *  * main -entry point
+ *   *
+ *    * Return: 0 after printing the function
+ *     */
+#include <stdio.h>
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+			puts("\"Programming is like building a multilingual puzzle");
+						return (0);
 }
+

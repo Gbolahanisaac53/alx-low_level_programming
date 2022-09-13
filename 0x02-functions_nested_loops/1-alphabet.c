2b63bc3821a8a557df6fdf4e2e6f58af35d4c968
Gbolahanisaac53 <isaacgbolahan53@gmail.com>
2022-09-13 16:24:52 -0700

    alphabet c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
  * main - alphabet
  * print_alphabet_x10 - print a - z ten times
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-		int i;
-		char j;
+	int i;
+	char j;
 
-		for (i = 0; I < 10; I++);
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			for(j = 'a'; j<= 'z'; j++);
-			{
-				_putchar(j);
-			}
-			putchar('\n');
+			_putchar(j);
 		}
+		_putchar('\n');
+	}
 }

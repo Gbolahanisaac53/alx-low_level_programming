457b50263567e221ef6d1abd9a9e771c4897aaa6
Gbolahan Isaac <111004367+Gbolahanisaac53@users.noreply.github.com>
2022-09-15 14:27:53 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,3 @@
-#include <unistd.h>
-
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -19,3 +9,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);

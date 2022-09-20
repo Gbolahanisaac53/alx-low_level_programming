a5914e851969dc81e402a347e26f3826c4f0c1a4
Gbolahanisaac53 <isaacgbolahan53@gmail.com>
2022-09-20 00:25:58 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 /**
- * reset_to_98 - this function update the value to 98
- *
- * @n: value
- */
+* reset_to_98 - this function update the value to 98
+*
+* @n: value
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
